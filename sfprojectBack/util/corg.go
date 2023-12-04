833e1c484814858a824bac3f3b811e27eb3f563d
say7fish <BaguHo@gmail.com>
2023-12-04 12:37:26 +0900

    update go cors func

--- sfprojectBack/util/corg.go
+++ sfprojectBack/util/corg.go
@@ -5,6 +5,7 @@ import "net/http"
 // CORS 관련 함수 정의
 func EnableCors(w *http.ResponseWriter) {
 	(*w).Header().Set("Access-Control-Allow-Origin", "*")
-	(*w).Header().Set("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE, OPTIONS")
-	(*w).Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
+    (*w).Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
+    (*w).Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, Range")
+    (*w).Header().Set("Access-Control-Expose-Headers", "Content-Length, Content-Range")
 }
