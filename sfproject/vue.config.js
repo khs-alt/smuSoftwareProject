911b704d00c673aa1c2da68267bc3bc569970d37
say7fish <BaguHo@gmail.com>
2023-12-04 11:04:19 +0900

    test

--- sfproject/vue.config.js
+++ sfproject/vue.config.js
@@ -1,14 +1,15 @@
 const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
   transpileDependencies: true,
+  publicPath: "/",
   devServer: {
     // port: 8080,
     // disableHostCheck: true
-    // proxy: {
-    //   "/": {
-    //     target: "http://localhost:8080",
-    //     // changeOrigin: true,
-    //   },
-    // },
+    proxy: {
+      "/": {
+        target: "http://localhost:8080",
+        //     // changeOrigin: true,
+      },
+    },
   },
 });
